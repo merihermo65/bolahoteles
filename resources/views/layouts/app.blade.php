8eb14a16852102aea42e8f1fc5e8a8228835223b
xY07x <jose.serna.2094@lacetania.cat>
2022-04-28 17:39:54 +0200

    update app.blade 1.0
    falta arreglar las rutas!!! :D

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'InstaLaravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,29 +21,29 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'InstaLaravel') }}
-                </a>
-                
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                @guest
 
-                
-              
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
+                @else
                     <ul class="navbar-nav ms-auto">
-                        
-                        <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home')}}" role="button">{{ __('Inici') }}</a></li> 
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home')}}" role="button">Reserva d'habitacions</a></li> 
+                        <li class="nav-item dropdown">
+                            <a id="hola" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{_('Restaurant') }}        
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="hola">
+                                        <a class="dropdown-item" href="{{ route('edit') }}">{{ __('Menu del dia') }}</a>
+                                        <a class="dropdown-item" href="{{ route('edita_pass') }}">{{ __('carta') }}</a>
+                            </div> 
+                        </li>
+                    </ul>
+                @endguest
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -57,22 +57,20 @@
                                 </li>
                             @endif
                         @else
-                        @if(Auth::user()->image)
-                                <div class="container-avatar">
-                                    <img src="{{ route('avatar', ['filename'=>Auth::user()->image])}}" class="avatar">
-                                </div>
-                        @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                @if(Auth::user()->image)
+                                    <img src="{{ route('avatar', ['filename'=>Auth::user()->image])}}" class="avatar" height="30px" width="35px" style="border-radius: 50%;">
+                                @endif
                                     {{ Auth::user()->name }}
-                                </a>
-                                
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     
-                                    <a class="dropdown-item" href="{{ route('edit') }}">
-                                        {{ __('Editar Perfil') }}
-                                    </a>
-                                   
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('edit') }}">{{ __('Editar perfil') }}</a>
+
+                                    <a class="dropdown-item" href="{{ route('edita_pass') }}">{{ __('Editar password') }}</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
