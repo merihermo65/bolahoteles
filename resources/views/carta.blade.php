ae391cac1a562756ef5d084fb7bbdb27a3a3e1a0
merihermo65 <104577061+merihermo65@users.noreply.github.com>
2022-05-03 20:23:54 +0200

    Revert "Merge branch 'master' of https://github.com/merihermo65/bolahoteles"
    This reverts commit 85fd40f3a37c0f0457113a0ee7cd8f0bed72946a, reversing
    changes made to b30d09f0a54b5b309633a955ac52e12e2f25480a.

--- resources/views/carta.blade.php
+++ resources/views/carta.blade.php
@@ -6,42 +6,10 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('La nostre Carta') }}</div>
-                    @foreach($datE as $value)
-                        <div class="card">
-                            <div class="card-body ">
-                                <div class="d-flex justify-content-center bg-image">
-                                    <img src="{{ route('getimageEE', ['filename'=>$value->image])}}" class="avatar" height="50%" width="50%">
-                                </div>
-                                <div>{{ __($value->description) }}</div>
-                            </div>
-                        </div><br>
-                    @endforeach
-            </div><br>
-            <div class="card">
-                <div class="card-header">Primer plato</div>
-                @foreach($datPP as $value)
-                <div class="card">
-                    <div class="card-body ">
-                        <div class="d-flex justify-content-center bg-image">
-                            <img src="{{ route('getimagePP', ['filename'=>$value->image])}}" class="avatar" height="50%" width="50%">
-                        </div>
-                        <div>{{ __($value->description) }}</div>
-                    </div>
-                </div><br>
-            @endforeach
-            </div><br>
-            <div class="card">
-                <div class="card-header">Primer plato</div>
-                @foreach($datP as $value)
-                <div class="card">
-                    <div class="card-body ">
-                        <div class="d-flex justify-content-center bg-image">
-                            <img src="{{ route('getimageP', ['filename'=>$value->image])}}" class="avatar" height="50%" width="50%">
-                        </div>
-                        <div>{{ __($value->description) }}</div>
-                    </div>
-                </div><br>
-            @endforeach
+
+                <div class="card-body">
+                    {{ __('Introduirem la carta pròximament!!') }}
+                </div>
             </div>
         </div>
     </div>
