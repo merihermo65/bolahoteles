e694fb5037d970533c83c9b46f579b57f063d6a9
merihermo65 <104577061+merihermo65@users.noreply.github.com>
2022-05-04 11:36:17 +0200

    Versión 1.3.6
    He tocado sobre todo el css

--- resources/views/carta.blade.php
+++ resources/views/carta.blade.php
@@ -3,42 +3,64 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <h1 style="text-align: center">LA CARTA</h1>
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('La nostre Carta') }}</div>
+                <div class="card-header">{{ __('Entrante') }}</div>
                     @foreach($datE as $value)
-                        <div class="card">
-                            <div class="card-body ">
-                                <div class="d-flex justify-content-center bg-image">
-                                    <img src="{{ route('getimageEE', ['filename'=>$value->image])}}" class="avatar" height="50%" width="50%">
+                    <div class="card">
+                        <div class="card-body " style="padding-bottom: 0px;">
+                            <div class="row">
+                                <div class="d-flex justify-content-center bg-image col">
+                                    <img src="{{ route('getimageEE', ['filename'=>$value->image])}}" height="70%" width="70%" style="float:right;padding-right: 20px;"> 
                                 </div>
-                                <div>{{ __($value->description) }}</div>
-                            </div>
-                        </div><br>
+                                <div class="col">
+                                    <h3>{{$value->name}}</h3>
+                                    {{ __($value->description) }}<br/><br/><br/><br/>
+                                        
+                                    Precio: {{$value->precio}}€
+                                </div>
+                                </div>
+                        </div>
+                    </div><br>
                     @endforeach
             </div><br>
             <div class="card">
                 <div class="card-header">Primer plato</div>
                 @foreach($datPP as $value)
                 <div class="card">
-                    <div class="card-body ">
-                        <div class="d-flex justify-content-center bg-image">
-                            <img src="{{ route('getimagePP', ['filename'=>$value->image])}}" class="avatar" height="50%" width="50%">
-                        </div>
-                        <div>{{ __($value->description) }}</div>
+                    <div class="card-body " style="padding-bottom: 0px;">
+                        <div class="row">
+                            <div class="d-flex justify-content-center bg-image col">
+                                <img src="{{ route('getimagePP', ['filename'=>$value->image])}}" height="70%" width="70%" style="float:right;padding-right: 20px;"> 
+                            </div>
+                            <div class="col">
+                                <h3>{{$value->name}}</h3>
+                                {{ __($value->description) }}<br/><br/><br/><br/>
+                                    
+                                Precio: {{$value->precio}}€
+                            </div>
+                            </div>
                     </div>
                 </div><br>
             @endforeach
             </div><br>
             <div class="card">
-                <div class="card-header">Primer plato</div>
+                <div class="card-header">Postre</div>
                 @foreach($datP as $value)
                 <div class="card">
-                    <div class="card-body ">
-                        <div class="d-flex justify-content-center bg-image">
-                            <img src="{{ route('getimageP', ['filename'=>$value->image])}}" class="avatar" height="50%" width="50%">
+                    <div class="card-body" style="padding-bottom: 0px;">
+                        <div class="row">
+                        <div class="d-flex justify-content-center bg-image col">
+                            <img src="{{ route('getimageP', ['filename'=>$value->image])}}" height="70%" width="70%" style="float:right;padding-right: 20px;"> 
                         </div>
-                        <div>{{ __($value->description) }}</div>
+                        <div class="col">
+                            <h3>{{$value->name}}</h3>
+                            {{ __($value->description) }}<br/><br/><br/><br/>
+                                
+                            Precio: {{$value->precio}}€
+                        </div>
+                        </div>
                     </div>
                 </div><br>
             @endforeach
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div style="main-content">
+    <div class="concept concept-one">
+        <div class="hover hover-1"></div>
+        <div class="hover hover-2"></div>
+        <div class="hover hover-3"></div>
+        <div class="hover hover-4"></div>
+        <div class="hover hover-5"></div>
+        <div class="hover hover-6"></div>
+        <div class="hover hover-7"></div>
+        <div class="hover hover-8"></div>
+        <div class="hover hover-9"></div>
+        <h1>RESTAURANT BOLA</h1>
+        <p>Has resrevat ja la teva taula? <br> Clica aquí i reserva-la facil i ràpidament<br><br><br><br><br><br> <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <defs>
+                <filter id="gooey">
+                    <!-- in="sourceGraphic" -->
+                    <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+                    <feColorMatrix in="blur" type="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="highContrastGraphic" />
+                    <feComposite in="SourceGraphic" in2="highContrastGraphic" operator="atop" />
+                </filter>
+            </defs>
+        </svg>
+        
+        <a id="gooey-button" href="{{ route('reservaDinar') }}">
+            Reservar Taula
+            <span class="bubbles">
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+            </span>
+        </a></p>
+        
+    </div>
+</div>
+
+
+
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8" style="padding-top: 20px;">
             <div class="card">
                 <div class="card-header">{{ __('Events Recents') }}</div>
 
@@ -15,7 +60,7 @@
                     @endif
                     @foreach($eventos as $evento)
                     <div style="border-radius:5px; border: 5px solid #402d15; background-color:#000000c9; border-bottom: 0;" class="card-header"> <div class="container-avatar">
-                    <h1 style="text-align: center; color:white">{{$evento->title}}</h1>
+                    <h2>{{$evento->title}}</h2>
                     
                     </div> </div>
                         <div class="card-body2" style=" color:white; background-color:#3a291087; border-radius:5px; border: 5px solid #402d15; margin-bottom: 25px;">
